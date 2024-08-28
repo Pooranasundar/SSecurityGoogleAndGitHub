9c8f627dc837fa4cf602294e18b0fccbf6b4c046
Pooranasundar <pooranasundar333@gmail.com>
2024-08-28 23:47:55 +0530

    Project Added

---
+++ src/main/java/com/example/SSecurityGoogleAndGitHub/SSecurityGoogleAndGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSecurityGoogleAndGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSecurityGoogleAndGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSecurityGoogleAndGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSecurityGoogleAndGitHub/SSecurityGoogleAndGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSecurityGoogleAndGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSecurityGoogleAndGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
